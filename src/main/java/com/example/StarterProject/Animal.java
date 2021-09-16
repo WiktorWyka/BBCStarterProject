b0aa334d31be50cad82a2f2118678796434098f6
Wiktor W <WiktorWyka@outlook.com>
2021-09-16 12:00:19 +0100

    Added new id field to Animal class to use in update endpoint

--- src/main/java/com/example/StarterProject/Animal.java
+++ src/main/java/com/example/StarterProject/Animal.java
@@ -4,13 +4,15 @@ import lombok.Data;
 
 @Data
 public class Animal {
+    private int id;
     private int numOfLegs;
     private String speciesName;
     private String soundMade;
 
-    public Animal(int newNumOfLegs, String newSpeciesName, String newSoundMade) {
+    public Animal(int newNumOfLegs, String newSpeciesName, String newSoundMade, int newId) {
         numOfLegs = newNumOfLegs;
         speciesName = newSpeciesName;
         soundMade = newSoundMade;
+        id = newId;
     }
 }
